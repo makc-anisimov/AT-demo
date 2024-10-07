29680768d7fd51b19f03d59d5fc16f971b291ce7
makc-anisimov <makc.anisimov@gmail.com>
2024-10-07 12:29:20 +0300

    complete app and  header for desktop and mobile resolutions

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,8 +1,38 @@
+import clsx from "clsx";
 import styles from "./header.module.scss";
 export const Header = () => {
   return (
     <header className={styles.header}>
-      
+      <button className={styles.header__mobMenuBtn} />
+      <h1 className={styles.header__title}>Setting</h1>
+      <div className={styles.header__menu}>
+        <div className={styles.header__personal}>
+          <input
+            className={styles.header__searchInput}
+            placeholder="Search for something"
+          />
+          <button className={styles.header__button}>
+            <div
+              className={clsx(
+                styles.header__buttonImage,
+                styles.header__buttonImage_settings
+              )}
+            />
+          </button>
+          <button className={styles.header__button}>
+            <div
+              className={clsx(
+                styles.header__buttonImage,
+                styles.header__buttonImage_notification
+              )}
+            />
+          </button>
+        </div>
+        <button
+        className={styles.header__avatar}
+      />
+      </div>
+
     </header>
   );
 };
